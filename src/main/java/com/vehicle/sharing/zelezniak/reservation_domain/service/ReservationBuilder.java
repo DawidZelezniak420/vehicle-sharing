87dbbbf98abdf5db3ffd30b4584052aa2b3b6f49
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-07-24 12:59:20 +0200

    new class ReservationBuilder in reservation service

---
+++ src/main/java/com/vehicle/sharing/zelezniak/reservation_domain/service/ReservationBuilder.java
@@ -0,0 +1,58 @@
+package com.vehicle.sharing.zelezniak.reservation_domain.service;
+
+import com.vehicle.sharing.zelezniak.common_value_objects.Money;
+import com.vehicle.sharing.zelezniak.reservation_domain.model.Reservation;
+import com.vehicle.sharing.zelezniak.reservation_domain.model.util.ReservationCreationRequest;
+import com.vehicle.sharing.zelezniak.user_domain.service.ClientService;
+import com.vehicle.sharing.zelezniak.vehicle_domain.model.vehicles.Vehicle;
+import com.vehicle.sharing.zelezniak.vehicle_domain.service.VehicleService;
+import com.vehicle.sharing.zelezniak.vehicle_domain.service.VehicleValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+class ReservationBuilder {
+
+    private final ClientService clientService;
+    private final VehicleValidator vehicleValidator;
+    private final VehicleService vehicleService;
+    private final ReservationCalculator reservationCalculator;
+
+    Reservation buildReservation(
+            ReservationCreationRequest request) {
+        Reservation basic = request.getReservation();
+        Reservation reservation = basic.toBuilder()
+                .vehicles(handleGetActiveVehicles(
+                        request.getVehiclesIds()))
+                .client(clientService.findById(
+                        request.getClientId()))
+                .reservationStatus(Reservation.ReservationStatus.ACTIVE)
+                .build();
+        calculateAndSetEstimatedCost(reservation);
+        return reservation;
+    }
+
+    private Set<Vehicle> handleGetActiveVehicles(
+            Set<Long> vehiclesIds) {
+        Collection<Vehicle> vehiclesByIDs = vehicleService.findVehiclesByIDs(
+                vehiclesIds);
+        Set<Vehicle> vehicles = new HashSet<>(vehiclesByIDs);
+        validateVehicles(vehicles);
+        return vehicles;
+    }
+
+    private void validateVehicles(Collection<Vehicle> vehicles) {
+        vehicleValidator.checkIfVehiclesAreActive(
+                vehicles);
+    }
+
+    private void calculateAndSetEstimatedCost(Reservation reservation) {
+        Money c = reservationCalculator.calculateCost(reservation);
+        reservation.setEstimatedCost(c);
+    }
+}
