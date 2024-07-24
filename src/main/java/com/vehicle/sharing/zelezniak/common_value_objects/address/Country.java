290b86f1229707c1884871674b39890706663649
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-07-24 16:38:31 +0200

    Country has been removed from project

---
+++ src/main/java/com/vehicle/sharing/zelezniak/common_value_objects/address/Country.java
@@ -0,0 +1,19 @@
+package com.vehicle.sharing.zelezniak.common_value_objects.address;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import static com.vehicle.sharing.zelezniak.constants.ValidationMessages.CAN_NOT_BE_BLANK;
+
+@Embeddable
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+@EqualsAndHashCode
+@ToString
+public class Country {
+
+    @NotBlank(message = "Country name" + CAN_NOT_BE_BLANK)
+    private final String countryName;
+}
