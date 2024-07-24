6246c1e435c69acc8ca23f52c0387e0f7695709a
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-07-24 12:54:02 +0200

    RentInformation uses RentDuration field instead LocalDateTime

--- src/main/java/com/vehicle/sharing/zelezniak/common_value_objects/RentInformation.java
+++ src/main/java/com/vehicle/sharing/zelezniak/common_value_objects/RentInformation.java
@@ -1,12 +1,9 @@
-package com.vehicle.sharing.zelezniak.rent_domain.model.rent_value_objects;
+package com.vehicle.sharing.zelezniak.common_value_objects;
 
+import com.vehicle.sharing.zelezniak.rent_domain.model.rent_value_objects.Location;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import lombok.*;
-
-import java.time.LocalDateTime;
 
 @Embeddable
 @Getter
@@ -17,13 +14,8 @@ import java.time.LocalDateTime;
 @ToString
 public class RentInformation {
 
-    private static final String MUST_BE_IN_FUTURE = " must be in future";
-
-    @FutureOrPresent(message = "Rental start" + MUST_BE_IN_FUTURE)
-    private final LocalDateTime rentalStart;
-
-    @Future(message = "Rental end" + MUST_BE_IN_FUTURE)
-    private final LocalDateTime rentalEnd;
+    @Embedded
+    private final RentDuration rentDuration;
 
     @Embedded
     private final Location pickUpLocation;
