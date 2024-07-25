ba9a5c3b5770c429edb5121d3f7dad093d449379
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-07-25 13:46:15 +0200

    service for processing new reservations in memory

--- src/main/java/com/vehicle/sharing/zelezniak/reservation_domain/service/NewReservationService.java
+++ src/main/java/com/vehicle/sharing/zelezniak/reservation_domain/service/NewReservationService.java
@@ -1,27 +1,60 @@
 package com.vehicle.sharing.zelezniak.reservation_domain.service;
 
-import com.vehicle.sharing.zelezniak.common_value_objects.Money;
 import com.vehicle.sharing.zelezniak.reservation_domain.model.Reservation;
-import com.vehicle.sharing.zelezniak.reservation_domain.model.util.ReservationCreationRequest;
+import com.vehicle.sharing.zelezniak.user_domain.model.client.Client;
 import com.vehicle.sharing.zelezniak.user_domain.service.ClientService;
-import com.vehicle.sharing.zelezniak.vehicle_domain.model.vehicles.Vehicle;
 import com.vehicle.sharing.zelezniak.vehicle_domain.service.VehicleService;
 import com.vehicle.sharing.zelezniak.vehicle_domain.service.VehicleValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-@Component
+@Service
 @RequiredArgsConstructor
-class ReservationBuilder {
+class NewReservationService {
 
     private final ClientService clientService;
     private final VehicleValidator vehicleValidator;
     private final VehicleService vehicleService;
     private final ReservationCalculator reservationCalculator;
 
+    private final Map<Long, Reservation> newReservations = new HashMap<>();
+    private Long idCounter = 0L;
 
+    public Reservation addNewReservation(
+            Long clientId) {
+        Client client = clientService.findById(clientId);
+        Reservation reservation = buildReservationForClient(client);
+        newReservations.put(++idCounter, reservation);
+        return reservation;
+    }
+
+    public Reservation updateReservation(
+            Long id,Reservation newData){
+     checkIfKeyExists(id);
+     newReservations.put(id,newData);
+     return newData;
+    }
+
+    public void removeReservation(Long id){
+        newReservations.remove(id);
+    }
+
+    private Reservation buildReservationForClient(
+            Client client) {
+        return Reservation.builder()
+                .id(idCounter)
+                .client(client)
+                .reservationStatus(Reservation.ReservationStatus.NEW)
+                .build();
+    }
+
+    private void checkIfKeyExists(Long id) {
+        if(!newReservations.containsKey(id)){
+            throw new IllegalArgumentException(
+                    "Reservation not found");
+        }
+    }
 }
